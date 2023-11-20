bcfff321f71dbef6ffedd5afba024869c43e3ca3
Nikhil Rout <135248190+NikhilRout@users.noreply.github.com>
2023-11-20 17:55:43 +0530

    Update DDA-8Bit-3BCD.v

--- DDA-8Bit-3BCD.v
+++ DDA-8Bit-3BCD.v
@@ -8,13 +8,13 @@ module DDA_8Bit_3BCD(
     shift_reg[7:0] = in_8bit;
     shift_reg[19:8] = 0;
     for (i = 0; i<8; i=i+1) begin
-    if(shift_reg[11:8] > 4)
-      shift_reg[11:8] = shift_reg[11:8] + 3;
-    if(shift_reg[15:12] > 4)
-      shift_reg[15:12] = shift_reg[15:12] + 3;
-    if(shift_reg[19:16] > 4)
-      shift_reg[19:16] = shift_reg[19:16] + 3;
-    shift_reg = shift_reg << 1;
+      if(shift_reg[11:8] > 4)
+        shift_reg[11:8] = shift_reg[11:8] + 3;
+      if(shift_reg[15:12] > 4)
+        shift_reg[15:12] = shift_reg[15:12] + 3;
+      if(shift_reg[19:16] > 4)
+        shift_reg[19:16] = shift_reg[19:16] + 3;
+      shift_reg = shift_reg << 1;
     end
    end
    assign out_3BCD = shift_reg[19:8];
