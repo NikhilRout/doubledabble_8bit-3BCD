9ed5d6e364abef992ae130002c3e1442b9c2873c
NikhilRout <nikhilrout97@gmail.com>
2023-12-07 01:29:30 +0530

    renames

--- DDA-8bit-3BCD_testbench.v
+++ DDA-8bit-3BCD_testbench.v
@@ -4,7 +4,7 @@ module DDA_8Bit_3BCD_testbench;
   DDA_8Bit_3BCD uut(.in_8bit(in_8bit), .out_3BCD(out_3BCD));
   initial begin
     $monitor("8-Bit Input: %b, BCD Output: %d%d%d", in_8bit, out_3BCD[11:8], out_3BCD[7:4], out_3BCD[3:0]);
-    in_8bit = 8'b00110101;
+    in_8bit = 8'b10101101;
     #100;
     $stop;
   end
